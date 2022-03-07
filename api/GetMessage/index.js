6b824a723a26c0e21aa462ff1ea7dbb343db0f5e
mkarmark <mikarmar@microsoft.com>
2022-03-07 11:56:47 -0800

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -2,5 +2,11 @@ module.exports = async function (context, req) {
   context.res = {
     // status: 200, /* Defaults to 200 */
     body: { text: "Hello Mitali!!" },
+    cookies: [{
+        name: 'HelloWorld',
+        value: 'abc',
+        path: '/',
+        sameSite: 'Lax',
+    }]
   };
 };
