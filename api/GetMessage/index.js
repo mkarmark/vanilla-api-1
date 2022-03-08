d8891ce0316fc7ed8d41aa4857214436e521bebe
mkarmark <mikarmar@microsoft.com>
2022-03-08 14:43:27 -0800

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -7,6 +7,23 @@ module.exports = async function (context, req) {
         value: 'abc',
         path: '/',
         sameSite: 'Lax',
+    },
+             {
+        name: 'HelloWorld2',
+        value: 'abc;2',
+        path: '/',
+        sameSite: 'Strict',
+    },
+             {
+        name: 'HelloWorld3',
+        value: 'abc3=',
+        path: '/',
+        sameSite: 'None',
+    },
+             {
+        name: 'HelloWorld4',
+        value: 'abc4',
+        path: '/',,
     }]
   };
 };
